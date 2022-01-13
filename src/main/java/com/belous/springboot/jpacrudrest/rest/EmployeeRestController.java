3aac4375d059c987a329cc769e8585f99c9828f1
BelousAnton <anton.belous@customertimes.com>
2022-01-13 22:12:58 +0300

    Implement Update Employee

--- src/main/java/com/belous/springboot/jpacrudrest/rest/EmployeeRestController.java
+++ src/main/java/com/belous/springboot/jpacrudrest/rest/EmployeeRestController.java
@@ -49,4 +49,11 @@ public class EmployeeRestController {
 
         return theEmployee;
     }
+
+    // add mapping for PUT "/employees" - update existing employee
+    @PutMapping("/employees")
+    public Employee updateEmployee(@RequestBody Employee theEmployee) {
+        employeeService.save(theEmployee);
+        return theEmployee;
+    }
 }
