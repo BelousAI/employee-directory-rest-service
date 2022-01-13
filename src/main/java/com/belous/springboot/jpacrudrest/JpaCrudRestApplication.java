51eeb3b3b004263c62cd1b92a4e7f6aa12a26cb1
BelousAnton <anton.belous@customertimes.com>
2022-01-13 10:32:54 +0300

    Init project

---
+++ src/main/java/com/belous/springboot/jpacrudrest/JpaCrudRestApplication.java
@@ -0,0 +1,13 @@
+package com.belous.springboot.jpacrudrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaCrudRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaCrudRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/belous/springboot/jpacrudrest/JpaCrudRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.belous.springboot.jpacrudrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaCrudRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
